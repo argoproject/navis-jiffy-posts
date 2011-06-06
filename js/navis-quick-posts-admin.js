9f1d67980b9952fd3bdebb9ddd21ee0c56266829
Marc Lavallee <mlavallee@gmail.com>
2011-06-06 12:29:54 -0400

    Restructured Navis_Quick_Posts to be a class. Moved JavaScript out to its own include.

---
+++ js/navis-quick-posts-admin.js
@@ -0,0 +1,69 @@
+function buildHtmlFromOembed( oembed ) {
+    var html = '';
+    
+    switch ( oembed.type ) {
+        case 'video':
+        case 'rich':
+        case 'html':
+            html = oembed.html;
+            break;
+        case 'link':
+            html = renderLinkOembed( oembed );
+            break;
+        case 'photo':
+            html = renderPhotoOembed( oembed );
+            break;
+        case 'error':
+        default:
+            alert( 'error' );
+            break;
+    }
+
+    return html;
+}
+
+
+function renderLinkOembed( oembed ) {
+    var html = '';
+    if ( oembed.thumbnail_url ) {
+        html += '<div class="module image left" width="' + 
+            oembed.width + '">';
+        html += '<img src="' + oembed.thumbnail_url + '" height="' +
+            oembed.height + '" width="' + oembed.width + '" ' +
+            'class="alignleft" />';
+        html += '</div>';
+    }
+    html += '<p><blockquote>' + oembed.description + '</blockquote></p>';
+    return html;
+}
+
+
+function renderPhotoOembed( oembed ) {
+    var html = '';
+    html += '<img src="' + oembed.url + '" />';
+    return html;
+}
+
+
+jQuery( document ).ready( function() {
+    var $ = jQuery;
+    $( '#submitUrl' ).click( function( evt ) {
+        var url = $( '#navis_embed_url' ).val();
+        var key = '6097efc282fa11e088ae4040f9f86dcd';
+        $.embedly( url, {maxWidth: 620, key: key}, function( oembed, dict ) {
+            // Set the title. Focus & blur necessary to wipe out 
+            // default "Enter title here" text
+            $( '#title' ).val( oembed.title );
+            $( '#title' ).focus(); $( '#title' ).blur();
+
+            // Get the generated HTML or make it ourselves
+            html = buildHtmlFromOembed( oembed );
+
+            // Add the embed to the TinyMCE visual editor
+            window.send_to_editor( html );
+
+            // Also, show a preview of the output
+            $( '#embedlyPreviewArea' ).html( html );
+        });
+    });
+});
--- quick-posts.php
+++ quick-posts.php
@@ -23,27 +23,114 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function navis_qp_register_post_types() {
-    register_post_type( 'quickpost', array(
-        'labels' => array(
-            'name' => 'Quick Posts',
-            'singular_name' => 'Quick Post',
-            'add_new' => 'Add New',
-            'add_new_item' => 'Add New Quick Post',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit Quick Post',
-            'view' => 'View',
-            'view_item' => 'View Quick Post',
-            'search_items' => 'Search Quick Posts',
-            'not_found' => 'No quick posts found',
-            'not_found_in_trash' => 'No quick posts found in Trash',
-        ),
-        'description' => 'Quick Posts',
-        'supports' => array( 'title', 'editor', 'comments', 'author' ),
-        'public' => true,
-        'menu_position' => 6,
-        'taxonomies' => array(),
-    ) );
-}
-add_action( 'init', 'navis_qp_register_post_types' );
+class Navis_Quick_Posts {
+    function __construct() {
+        add_action( 'init', array( &$this, 'register_post_type' ) );
 
+        add_action( 'admin_print_scripts-post.php', 
+            array( &$this, 'register_admin_scripts' )
+        );
+        add_action( 'admin_print_scripts-post-new.php', 
+            array( &$this, 'register_admin_scripts' )
+        );
+
+        add_action( 'admin_menu', array( &$this, 'add_post_meta_boxes' ) );
+
+        add_action( 'admin_menu', array( &$this, 'add_options_page' ) );
+    }
+
+
+    function register_post_type() {
+        register_post_type( 'quickpost', array(
+            'labels' => array(
+                'name' => 'Quick Posts',
+                'singular_name' => 'Quick Post',
+                'add_new' => 'Add New',
+                'add_new_item' => 'Add New Quick Post',
+                'edit' => 'Edit',
+                'edit_item' => 'Edit Quick Post',
+                'view' => 'View',
+                'view_item' => 'View Quick Post',
+                'search_items' => 'Search Quick Posts',
+                'not_found' => 'No quick posts found',
+                'not_found_in_trash' => 'No quick posts found in Trash',
+            ),
+            'description' => 'Quick Posts',
+            'supports' => array( 'title', 'editor', 'comments', 'author' ),
+            'public' => true,
+            'menu_position' => 6,
+            'taxonomies' => array(),
+        ) );
+    }
+
+
+    function register_admin_scripts() {
+        if ( 'quickpost' != get_post_type() )
+            return;
+
+        // Embed.ly's JavaScript client
+        $libsrc = plugins_url( 
+            'js/embedly-jquery/jquery.embedly.js', __FILE__ 
+        );
+        wp_enqueue_script( 'jquery-embedly', $libsrc, 
+            array( 'jquery' ), '2.0.0' 
+        );
+
+        // Our JS routines
+        $oursrc = plugins_url( 'js/navis-quick-posts-admin.js', __FILE__ );
+        wp_enqueue_script( 'navis-quick-posts-admin', $oursrc, 
+            array( 'jquery-embedly' ), '0.1' 
+        );
+    }
+
+
+    function add_post_meta_boxes() {
+        add_meta_box( 'navisembedurl', 'Embed a URL', 
+            array( &$this, 'embed_url_box' ), 'quickpost', 
+            'side', 'core' 
+        );
+
+        add_meta_box( 'navisembedpreview', 'Preview Embed',
+            array( &$this, 'embed_preview_box' ), 'quickpost',
+            'normal', 'high'
+        );
+    }
+
+
+    function embed_url_box() {
+    ?>
+        URL: <input type="text" name="navis_embed_url" id="navis_embed_url" value="" style="width: 80%;" />
+        <input type="button" class="button" id="submitUrl" value="Embed" label="Embed" />
+    <?php
+    }
+
+
+    function embed_preview_box() {
+    ?>
+        <div id="embedlyPreviewArea"></div>
+    <?php
+    }
+
+    function add_options_page() {
+        add_options_page( 'Quick Posts', 'Quick Posts', 'manage_options',
+                          'navis_qp', array( &$this, 'options_page' ) );
+    }
+
+
+    function options_page() {
+    ?>
+        <div>
+            <h2>Navis Quick Posts</h2>
+            <form action="options.php" method="post">
+                <?php settings_fields( 'navis_qp' ); ?>
+                <?php do_settings_sections( 'navis_qp' ); ?>
+
+                <input name="Submit" type="submit" value="<?php esc_attr( 'Save Changes' ); ?>" />
+            </form>
+        </div>
+    <?php
+    }
+
+}
+
+new Navis_Quick_Posts;
